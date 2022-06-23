d331f6e5c66e4de62bb3b46e75a574ad462095fe
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-06-23 09:52:12 +0300

    Add close menu event

--- js/mobilemenu.js
+++ js/mobilemenu.js
@@ -1,3 +1,12 @@
 const toggleMenuButton = document.getElementById('toggle-menu');
 const menu = document.querySelector('.mobile-menu');
+const closeButton = document.querySelector('.close-icon');
 
+toggleMenuButton.addEventListener('click', () => {
+  toggleMenuButton.classList.toggle('active');
+  menu.classList.toggle('active');
+});
+
+closeButton.addEventListener('click', () => {
+  menu.classList.remove('active');
+});
